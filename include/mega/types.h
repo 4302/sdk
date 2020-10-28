a87f2b2b027322471c6afd41bc69f56a2a480958
David Ward <dw@mega.co.nz>
2020-10-28 14:21:01 +1300

    Added support for (ex|in)ternal syncs.

--- include/mega/types.h
+++ include/mega/types.h
@@ -971,6 +971,10 @@ public:
     handle getBackupId() const;
     void setBackupId(const handle &backupId);
 
+    // Whether this sync is backed by an external device.
+    void isExternal(bool isExternal);
+    bool isExternal() const;
+
 private:
 
     // Unique identifier. any other field can change (even remote handle),
@@ -979,6 +983,9 @@ private:
 
     // enabled/disabled by the user
     bool mEnabled = true;
+
+    // sync stored on external device.
+    bool mExternal = false;
 
     // the local path of the sync
     std::string mLocalPath;
